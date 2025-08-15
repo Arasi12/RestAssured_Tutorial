2ade668f899bd0d3bdf2cac161c4c754148d2320
Lenovo <Lenovo@LAPTOP-VQC12HNU>
2025-08-15 20:11:04 +0530

    second commit

--- RestAssured_Tutorial/src/test/java/com/rest/nonBDD/getAllEmployees.java
+++ RestAssured_Tutorial/src/test/java/com/rest/nonBDD/getAllEmployees.java
@@ -3,7 +3,7 @@ package com.rest.nonBDD;
 public class getAllEmployees {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello API");
 
 	}
 
