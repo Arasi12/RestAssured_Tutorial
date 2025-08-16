a022e3044f905526d192fb8ee08d55816f54f0ce
Lenovo <Lenovo@LAPTOP-VQC12HNU.bbrouter>
2025-08-16 17:53:52 +0530

    new commit

--- RestAssured_Tutorial/src/test/java/com/rest/nonBDD/updateAnEmployee.java
+++ RestAssured_Tutorial/src/test/java/com/rest/nonBDD/updateAnEmployee.java
@@ -7,22 +7,24 @@ import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class createAnEmployee {
+public class updateAnEmployee {
 
 	@Test
-	public void CreateAnEmployee() {
+	public void updateEmployee() {
 		RestAssured.baseURI="http://localhost:3000/";
-		RequestSpecification requestSpecification=	RestAssured.given().
+		RequestSpecification requestSpecification= RestAssured.given().
 				body("{\r\n"
-						+ "    \"title\": \"Selenium\",\r\n"
-						+ "    \"author\": \"Arasi\"\r\n"
-						+ "}"
-						);
-		Response response= requestSpecification.request(Method.POST,"posts");
+						+ "    \"id\": \"342\",\r\n"
+						+ "    \"title\": \"APITEST16aug\",\r\n"
+						+ "    \"author\": \"Arasi M\"\r\n"
+						+ "  }");
+		Response response=requestSpecification.request(Method.PUT,"posts/79eb");
 		System.out.println(response.statusCode());
 		System.out.println(response.statusLine());
 		System.out.println(response.asPrettyString());
 		
+		
 	}
-
+	
+	
 }
