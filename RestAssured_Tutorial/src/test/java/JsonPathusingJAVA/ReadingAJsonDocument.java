08a5313f10de30c9fa95b6383df5eccd406eff6b
Lenovo <Lenovo@LAPTOP-VQC12HNU>
2025-09-01 16:26:24 +0530

    test

--- RestAssured_Tutorial/src/test/java/JsonPathusingJAVA/ReadingAJsonDocument.java
+++ RestAssured_Tutorial/src/test/java/JsonPathusingJAVA/ReadingAJsonDocument.java
@@ -1,11 +1,45 @@
 package JsonPathusingJAVA;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.List;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.InvalidJsonException;
+import com.jayway.jsonpath.JsonPath;
+
 public class ReadingAJsonDocument {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void ReadingAJSONDocument() throws IOException {
+		File file=new File("src/test/resources/BookStore.json");
+		List<Object> allcategory=JsonPath.read(file, "$..category");
 		
+		for (Object category : allcategory) {
+			System.out.println(category);
+		}
+		
+	}
+	
+	public static void ReadingAJSONDocumentParseoneTime() throws InvalidJsonException, IOException {
+		InputStream file=new FileInputStream("src/test/resources/BookStore.json");
+		
+		Object	Parsedjsondoc= Configuration.defaultConfiguration().
+		jsonProvider().parse(file.readAllBytes());
+		
+		List<Object> allcategory=JsonPath.read(Parsedjsondoc, "$..category");
+		for (Object category : allcategory) {
+			System.out.println(category);
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		//ReadingAJSONDocument();
+		ReadingAJSONDocumentParseoneTime();
 	}
 
 }
